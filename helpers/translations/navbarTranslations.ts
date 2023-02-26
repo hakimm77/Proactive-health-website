b4b6fae85f6ee6d04c623165a29b168e540a37ce
hakimm77 <hakimboum406@gmail.com>
2023-02-26 19:38:32 +0100

    translated navbar items

--- helpers/translations/navbarTranslations.ts
+++ helpers/translations/navbarTranslations.ts
@@ -2,4 +2,21 @@ export const navbarTranslations = {
   "BOKA KONSULTATION": "BOOK CONSULTATION",
   "Om oss": "About us",
   Tjänster: "Services",
+  Forskning: "Research",
+  //navItems items
+  Personal: "Staff",
+  Styrelse: "Board",
+  Stiftelse: "Foundation",
+  Investerare: "Investor",
+
+  Basprover: "Base samples",
+  Överviktsbehandling: "Obesity treatment",
+  Receptförnyelse: "Prescription renewal",
+
+  "Egen forskning": "Own research",
+  Referensartiklar: "Reference articles",
+
+  Nyheter: "News",
+  Pressmedelanden: "Press releases",
+  Prenumerera: "Subscribe",
 };
